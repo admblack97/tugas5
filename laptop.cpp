ede28524a4f407201060099575e9d27b101cd188
admblack97 <admblack97@gmail.com>
2018-01-19 00:11:35 +0800

    behhhh mau sdh

--- laptop.cpp
+++ laptop.cpp
@@ -5,8 +5,16 @@ using namespace std;
 class laptop{
 	public:
 		void bemainan(string main);
+		string tipe;
 };
 
 int main () {
-	
+	laptop laptopAsus, laptopAcer;
+	laptopAsus.tipe = "ASUS ROG ZEPHYRUS (GX501) \n";
+	cout<<(laptopAsus.tipe);                                            
+	laptopAsus.bemainan("Kawa bemainan game barat, kaya The Witcher 3, Final Fnatasy XV dll");
+}
+
+void laptop::bemainan(string main){
+	cout<<main<<", harat kalo :D \n";
 }
